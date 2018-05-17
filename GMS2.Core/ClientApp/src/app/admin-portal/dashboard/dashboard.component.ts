1a46fd9e6d32435d30f4bc47ca54391794fb2d13
Eric T <ericson.t@hotmail.com>
2018-05-17 19:18:08 +1000

    Admin Portal Dash Components
    cal, finance, notifications

--- GMS2.Core/ClientApp/src/app/admin-portal/dashboard/dashboard.component.ts
+++ GMS2.Core/ClientApp/src/app/admin-portal/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+import 'fullcalendar';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  summary = [
+    { user: 'n/a', type: 'Credit', description: 'Lesson', amount: 80 },
+    { user: 'n/a', type: 'Credit', description: 'Lesson', amount: 60 },
+    { user: 'n/a', type: 'Debit', description: 'Wage', amount: 250 },
+    { user: 'n/a', type: 'Credit', description: 'Instrument Sale', amount: 650 },
+  ]
+
   constructor() { }
 
   ngOnInit() {
+    $('#calendar').fullCalendar({
+      eventLimit: false,
+      header: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'month,agendaWeek,agendaDay'
+      },
+      // events: this.events,
+      // eventClick: this.eventClick
+    });
   }
 
 }
